9b5700b20bf36ec48033c2b21feab3ebf30201c4
Yovador <yovador@gmail.com>
2021-06-24 14:59:19 +0200

    fond vert sur message

--- Assets/Scripts/ConversationButtons.cs
+++ Assets/Scripts/ConversationButtons.cs
@@ -11,14 +11,25 @@ public class ConversationButtons : MonoBehaviour
     protected int waitIncrement = 0;
     protected int waitLimit = 60;
     protected TMP_Text myText;
+    protected RectTransform completionBackground;
+    protected Vector3 defaultPosition;
 
     virtual protected void Start()
     {
         conversationDisplayer = GameObject.Find("ConversationDisplayer").GetComponent<ConversationDisplayer>();
         myText = GetComponentInChildren<TMP_Text>();
+        completionBackground = transform.Find("CompletionMask").Find("CompletionBackground").GetComponent<RectTransform>();
+
+        StartCoroutine(WaitOneFrame());
 
     }
+    protected IEnumerator WaitOneFrame()
+    {
+        yield return null;
+        completionBackground.localPosition = new Vector3(0, completionBackground.rect.y, 0);
+        defaultPosition = new Vector3(0, completionBackground.rect.y, 0);
 
+    }
     private void Update()
     {
         if (isHold)
@@ -39,6 +50,9 @@ public class ConversationButtons : MonoBehaviour
         {
             waitIncrement++;
         }
+        int currentSize = Mathf.RoundToInt(((completionBackground.rect.y * waitIncrement) / waitLimit) + (-completionBackground.rect.y / 2));
+        //Debug.Log(currentSize);
+        completionBackground.localPosition = new Vector3(0, -currentSize, 0);
     }
 
     virtual public void OnPointerDown()
@@ -58,6 +72,8 @@ public class ConversationButtons : MonoBehaviour
         }
         else
         {
+            completionBackground.localPosition = defaultPosition;
+
             waitIncrement = 0;
         }
     }
